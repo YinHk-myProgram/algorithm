57288b0bd0c0b1ad9e683ffcc9bfe7eae9714364
Yin <67538811+YinHk@users.noreply.github.com>
2021-11-19 12:13:49 +0800

    Create sorting.js

---
+++ sorting algorithm/sorting.js
@@ -0,0 +1,9 @@
+for(let i=1;i<=nums.length-1;i++){
+      for(let j=i;j>=1;j--){
+        if(nums[j]<nums[j-1]){
+           let temp = nums[j-1];
+           nums[j-1] = nums[j];
+           nums[j] = temp;
+         } 
+       }
+}
