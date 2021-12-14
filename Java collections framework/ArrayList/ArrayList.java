ec912865ba6f5f6e0995676d14e09c923c175655
Yin <67538811+YinHk@users.noreply.github.com>
2021-12-14 23:53:40 +0800

    Update ArrayList.java

--- Java collections framework/ArrayList/ArrayList.java
+++ Java collections framework/ArrayList/ArrayList.java
@@ -1,8 +1,11 @@
+java.util.collections;  
+
 /* 
 Why ArrayList is better than Array?
 Because The limitation with array is that it has a fixed length so if it is full you cannot add any more elements to it,
 ArrayList can dynamically grow and shrink after addition and removal of elements, also we can use predefined methods of it which makes our task easy.
 */
+
 
 //declare an ArrayList
 ArrayList<T> alist = new ArrayList<T>();       //define your data type
@@ -10,10 +13,11 @@ ArrayList<T> alist = new ArrayList<T>();       //define your data type
 ArrayList<String> alist=new ArrayList<String>();    //string arraylist
 ArrayList<Integer> list=new ArrayList<Integer>();   //integer arraylist
 
-//add elements
+//Add elements
+alist.add("element");
 
-
-
+//Change an element
+alist
 
 
 
