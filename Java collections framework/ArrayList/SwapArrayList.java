c5ca03a40a93b2ca3fa74db461e2ac3ce99b5900
Yin <67538811+YinHk@users.noreply.github.com>
2021-12-15 23:36:05 +0800

    Update and rename swapArrayList.java to SwapArrayList.java

--- Java collections framework/ArrayList/SwapArrayList.java
+++ Java collections framework/ArrayList/SwapArrayList.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class swapArrayList {
+public class SwapArrayList {
   public Collection<T> swapElements(ArrayList<E> al, int el, int e2) {
       return Collections.swap(al, e1, e2);
   };
