bc95f3a269ac37743b03dadf5be16f864fe7e908
Yin <67538811+YinHk@users.noreply.github.com>
2021-12-15 16:14:09 +0800

    Update ArrayListUsage.java

--- Java collections framework/ArrayList/ArrayListUsage.java
+++ Java collections framework/ArrayList/ArrayListUsage.java
@@ -8,9 +8,24 @@
 //Using util package
 import java.util.*;
 
-//Declare an ArrayList and define your data type for the element inside the array, default capacity is 10
-ArrayList<T> list = new ArrayList<T>();
+//Declare an ArrayList and define your data type for the element inside the array
+ArrayList<T> list = new ArrayList<T>();      //default capacity is 10
+ArrayList<T> list2 = new ArrayList<T>(n);    //define capacity n   
+ArrayList<T> list3 = new ArrayList<T>(c);    //specific collection object
 
+/*  Initialization */
+ArrayList<T> arrlist = new ArrayList<T>( Arrays.asList( obj1, obj2, obj3,....) );     //using Arrays.asList
+ArrayList<T> arrlist2 = new ArrayList<T>() {{
+                        add(obj1);
+		                  add(obj2);
+		                  add(obj3);
+                        ... }};                                                       //using anonymous inner class
+ArrayList<T> arrlist3 = new ArrayList<T>();                                           //use the add method in ArrayList
+arrlist3.add(obj1);
+arrlist3.add(obj2);
+arrlist3.add(obj3);
+....
+   
 /* Add elements */
 
 //Add the specified element e to the end of this list. 
@@ -63,7 +78,7 @@ list.set(index, e);
 list.get(index);
    
 //Clone ArrayList, returns a shallow copy of ArrayList
-ArrayList list2 = (ArrayList) list.clone();   //list2 equals to list
+ArrayList copyList = (ArrayList) list.clone();   //list2 equals to list
 
 //Returns the index of the first occurrence of the specified element e
 list.indexOf(e);     //if not found, return -1
@@ -73,3 +88,5 @@ list.contains(e);    //return a boolean
 
 //Trims the capacity of this ArrayList instance to be the array list's current size.
 list.trimToSize();
+
+/
