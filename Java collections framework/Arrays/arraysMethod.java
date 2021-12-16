7bf798c927945d2a51fc52c99ea7add53d0db782
Yin <67538811+YinHk@users.noreply.github.com>
2021-12-16 13:26:29 +0800

    Update arraysMethod.java

--- Java collections framework/Arrays/arraysMethod.java
+++ Java collections framework/Arrays/arraysMethod.java
@@ -3,9 +3,15 @@
 //using the Arrays class in the java util package
 import java.util.*;
 
-int a[] = new int[n];  //n elements 
+int[] arr = new int[n];  //n elements 
 
 /* Methods */
 
 //Returns a fixed-size list backed by the specified array.
+List list = Arrays.aslist(a);
+
+//Searches the specified value by using the binary search algorithm
+int value1 = Arrays.binarySearch(arr, key);    //key: the searching value in the array 
+int value2 = Arrays.binarySearch(arr, fromInde, toIndex, key);    
+
 
