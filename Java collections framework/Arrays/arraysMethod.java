f9456969b59e24a155cb7b41fdf71ea677f46921
Yin <67538811+YinHk@users.noreply.github.com>
2021-12-16 13:16:22 +0800

    Update arraysMethod.java

--- Java collections framework/Arrays/arraysMethod.java
+++ Java collections framework/Arrays/arraysMethod.java
@@ -1,4 +1,11 @@
 // This class contains various usefule methods for manipulating arrays
 
 //using the Arrays class in the java util package
-import 
+import java.util.*;
+
+int a[] = new int[n];  //n elements 
+
+/* Methods */
+
+//Returns a fixed-size list backed by the specified array.
+
