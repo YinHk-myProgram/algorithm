5824e078986b4253b07b8cbf24062fcabe6f75b3
Yin <67538811+YinHk@users.noreply.github.com>
2022-01-05 15:44:11 +0800

    Update Delete.java

--- LinkedList/Delete.java
+++ LinkedList/Delete.java
@@ -1,1 +1,44 @@
 
+Node head; // head of list
+ 
+    /* Linked list Node*/
+    class Node {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+ 
+    /* Given a key, deletes the first
+       occurrence of key in
+     * linked list */
+    void deleteNode(int key) {
+      
+        // Store head node
+        Node temp = head, prev = null;
+ 
+        // If head node itself holds the key to be deleted
+        if (temp != null && temp.data == key) {
+            head = temp.next; // Changed head
+            return;
+        }
+ 
+        // Search for the key to be deleted, keep track of
+        // the previous node as we need to change temp.next
+        while (temp != null && temp.data != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+ 
+        // If key was not present in linked list
+        if (temp == null)
+            return;
+ 
+        // Unlink the node from linked list
+        prev.next = temp.next;
+    }
+ 
+    
