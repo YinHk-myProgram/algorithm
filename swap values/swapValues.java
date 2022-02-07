3437e63fc4482e4c7b6a25a0bbb2156f5cad5044
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-07 21:42:38 +0800

    Create swapValues.java

---
+++ swap values/swapValues.java
@@ -0,0 +1,35 @@
+//swap two values by using third variable
+public swap(int x, int y) {
+  
+  int temp;
+  temp = y;
+  y = x;
+  x = temp;
+  
+}
+
+
+//swap two numbers without using the third variable
+import java.util.*;  
+class Swap   
+{  
+    public static void main(String a[])   
+    {   
+        System.out.println("Enter the value of x and y");  
+      
+        Scanner sc = new Scanner(System.in);  
+      
+        /*Define variables*/  
+        int x = sc.nextInt();  
+        int y = sc.nextInt();  
+      
+        System.out.println("before swapping numbers: "+x +" "+ y);  
+      
+        /*Swapping*/  
+        x = x + y;   
+        y = x - y;   
+        x = x - y;   
+      
+        System.out.println("After swapping: "+x +"  " + y);   
+    }   
+}  
