a6aaa2343dac181874a3ddf15c7445711c77ead4
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-13 23:07:15 +0800

    Update swapValues.js

--- swap values/swap two values/swapValues.js
+++ swap values/swap two values/swapValues.js
@@ -1,3 +1,15 @@
 // swap two values by destructing assignment
 // [a, b] = [b, a] is the destructuring assignment that swaps the variables a and b.
 [a,b] = [b,a]
+
+
+// By Temporary variable
+let a = 1;
+let b = 2;
+let temp;
+
+temp = a;
+a = b;
+b = temp;
+
+
