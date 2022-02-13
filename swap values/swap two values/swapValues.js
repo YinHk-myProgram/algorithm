84c3449f33a0bc2859935bddd8e679dcdedd169b
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-13 23:13:23 +0800

    Update swapValues.js

--- swap values/swap two values/swapValues.js
+++ swap values/swap two values/swapValues.js
@@ -2,7 +2,6 @@
 // [a, b] = [b, a] is the destructuring assignment that swaps the variables a and b.
 [a,b] = [b,a]
 
-
 // By Temporary variable
 let a = 1;
 let b = 2;
@@ -12,4 +11,23 @@ temp = a;
 a = b;
 b = temp;
 
+// Addition and difference
+// swap variables without the use of additional memory (like a temporary array or variable). 
+// The following example swaps the variables a and b using the addition + and difference - arithmetic operators.
+let a = 1;
+let b = 2;
+
+a = a + b;
+b = a - b;
+a = a - b;
+// a => 2
+// b => 1
+
+// Bitwise XOR operator
+// the bitwise XOR operator n1 ^ n2 performs the XOR operation on each bit of n1 and n2 numbers.
+let a = 1;
+let b = 2;
+a = a ^ b;
+b = a ^ b;
+a = a ^ b;
 
