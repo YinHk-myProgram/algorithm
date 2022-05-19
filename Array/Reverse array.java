3fab30667de8d88920f10906880c15787bbf45b6
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-19 15:53:33 -0400

    Update Reverse array.java

--- Array/Reverse array.java
+++ Array/Reverse array.java
@@ -1,7 +1,36 @@
 //Reverse an array
-	for (int i=0;i<anArray.length-1;i++){
-		int temp = anArray[i];
-		anArray[i] = anArray[n-1-i];
-		anArray[n-1-i] = temp;
-	}
+
+/*
+   1) Initialize start and end indexes as start = 0, end = n-1 
+   2) In a loop, swap arr[start] with arr[end] and change start and end as follows : 
+	
+	  start = start +1, end = end – 1
+ */
+public void rvereseArray(int arr[], int start, int end){
+     int temp;
+     while (start < end) {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+     }
+} 
+
+
+/*  
+    1) Initialize start and end indexes as start = 0, end = n-1 
+    2) Swap arr[start] with arr[end] 
+    3) Recursively call reverse for rest of the array.
+ */
+public void rvereseArray(int arr[], int start, int end){
+        int temp;
+        if (start >= end)
+            return;
+	
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        rvereseArray(arr, start+1, end-1);
+}
 
