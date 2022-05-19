da38a7f9c467b6f390a4497c28287c16c80cca94
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-19 16:37:05 -0400

    Create LinearSearch.java

---
+++ Search/LinearSearch.java
@@ -0,0 +1,13 @@
+// Linear or Sequential Search
+
+public int search(int[] nums, int target) {
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] == target) {
+                return index;
+            }
+        }
+        return -1;
+}
+  
+  
+  
