9cc8683c0f183e7d876fb5cf9c9c9fdcfd13b8e6
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-20 15:38:15 -0400

    Create reverseString.js

---
+++ String Processing/Reverse string/reverseString.js
@@ -0,0 +1,30 @@
+// Reverse a String With Built-In Functions
+
+const reverseString = function(str) {
+  
+   // use the split() method to return a new array
+   let splitStringArr = str.split("");
+  
+   //  Use the reverse() method to reverse the new created array
+   let reverseArray = splitStringArr.reverse();
+   
+   // Use the join() method to join all elements of the array into a string
+   return reverseArray.join("");
+}
+
+
+// Reverse a String With a For Loop
+function reverseStr(str) {
+    let newString = "";
+  
+    for (let i=str.length - 1; i >= 0; i--) { 
+       newString += str[i];      // or newString = newString + str[i];
+    }
+    return newString; 
+}
+
+// Reverse a String With Recursion
+function reverseStri(str) {
+     return (str === '') ? '' : reverseStri(str.substr(1)) + str.charAt(0);
+}
+
