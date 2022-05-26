e93e24c38af59b74096dd5856d9c14f1f172048f
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-26 00:55:59 -0400

    Update SelectionSort.js

--- sorting/SelectionSort/SelectionSort.js
+++ sorting/SelectionSort/SelectionSort.js
@@ -28,3 +28,29 @@ function swap(A, x, y) {
     A[y] = temp;
 }
 
+
+
+function selectionSort(arr) { 
+    let n = arr.length;
+        
+    for(let i = 0; i < n; i++) {
+        // Finding the smallest number in the subarray, default index of min is in i
+        let min = i;
+        for(let j = i+1; j < n; j++){
+            if(arr[j] < arr[min]) {
+                min = j;             // change the index of min to j
+            }
+         }
+         
+         if (min != i) {
+             // Swapping the elements
+             let tmp = arr[i]; 
+             arr[i] = arr[min];
+             arr[min] = tmp;      
+        }
+    }
+    return arr;
+}
+
+
+
