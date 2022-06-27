c7eadd0b1c9129feb5378bb13b46efe3ba432357
Ken <67538811+YinHk@users.noreply.github.com>
2022-06-26 23:50:10 -0400

    Update combinations.js

--- combination & permutation/combinations.js
+++ combination & permutation/combinations.js
@@ -2,3 +2,38 @@
 
 // list all the combination of k numbers out of n numbers (assume [1 ... n])
 
+const combinations = (n, k) => {
+  let res = [];
+
+  const backtracking = (start, tempArr) => {
+    if (tempArr.length === k) {
+      res.push([...tempArr]);     //make copy of the reference
+      return;
+    }
+
+    for (let i = start; i < n + 1; i++) {
+      tempArr.push(i);
+      backtracking(i + 1, tempArr);
+      tempArr.pop();
+    }
+  };
+  backtracking(1, []);
+  return res;
+};
+
+
+//eg:
+combinations(5, 3);
+/*
+ [
+  [ 1, 2, 3 ], [ 1, 2, 4 ],
+  [ 1, 2, 5 ], [ 1, 3, 4 ],
+  [ 1, 3, 5 ], [ 1, 4, 5 ],
+  [ 2, 3, 4 ], [ 2, 3, 5 ],
+  [ 2, 4, 5 ], [ 3, 4, 5 ]
+ ]
+*/
+  
+
+
+
