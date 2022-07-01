2bd43f237f2be5854fc887a6752e137a5c2edc81
Ken <67538811+YinHk@users.noreply.github.com>
2022-07-01 12:56:40 -0400

    Update combination_sum.js

--- combination & permutation/combination sum/combination_sum.js
+++ combination & permutation/combination sum/combination_sum.js
@@ -1,8 +1,28 @@
 /* Find out the combination sum */
 
-//Input: candidates = [2,3,5], target = 8
-//Output: [[2,2,2,2],[2,3,3],[3,5]]
-// 
+// Input: candidates = [2,3,5], target = 8
+// Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+
+/* beack tracking method and checking remainder */ 
+const combinationSum = (nums, target) => {
+    let res = [];
+    nums.sort((a, b) => a-b);
+    
+    const backtracking = (start, tempArr, remainder) => {
+        for(let i=start; i<candidates.length&&candidates[i]<=remainder; i++) {
+            if(nums[i] === remainder)  res.push([...tempArr, nums[i]]);
+             else backtracking(i, [...tempArr, nums[i]],  remainder - nums[i]);     
+        }
+    }
+    backtracking(0, [], target);
+    return res;
+};
+
+--------------------------------------------------------------------------------------------
+
+
+/* beack tracking method and checking sum */ 
 var combinationSum = function(candidates, target) {
     let res = [];
     let index = 0;
