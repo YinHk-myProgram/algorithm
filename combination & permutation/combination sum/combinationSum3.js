94df62992cd445fbb3fd54372d9974d4e627aafa
Ken <67538811+YinHk@users.noreply.github.com>
2022-07-01 17:34:29 -0400

    Update combinationSum3.js

--- combination & permutation/combination sum/combinationSum3.js
+++ combination & permutation/combination sum/combinationSum3.js
@@ -6,6 +6,7 @@
 
 */ 
 
+// version 1
 const combinationSum3 = (k, n) => {
     let res = [];
     let list = [];
@@ -28,3 +29,34 @@ const dfs = function(k, n, result, list, sum, start){
         list.pop();
     }
 }   
+
+
+//version 2
+const combinationSum3 = (k, n) => {
+   const nums = [1,2,3,4,5,6,7,8,9];
+   let res = [];
+   
+   const backtracking = (start, tempArr, sum) => {
+      if(tempArr.length > k) return;
+      if(tempArr.length === k) {
+         if(sum === n) {
+             res.push([...tempArr]);
+             return;
+         }
+         return;
+      }
+       
+      for(let i=start; i<nums.length; i++) { 
+         tempArr.push(nums[i]);
+         backtracking(i+1, tempArr, sum + nums[i]);
+         tempArr.pop();
+      }  
+   }
+   
+   for(let i=0; i<nums.length; i++) {
+       backtracking(i+1, [nums[i]], nums[i]);
+   }
+   return res;  
+};
+
+
