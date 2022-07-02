bb5eb5d2e8d05e27172795f24db275ac0631990c
Ken <67538811+YinHk@users.noreply.github.com>
2022-07-02 15:48:51 -0400

    Create postorder-traversal.js

---
+++ binary tree/traversal/postorder-traversal.js
@@ -0,0 +1,14 @@
+const postorderTraversal = root => {
+    let list = [];
+    //recurrsion method
+    const postorder = node => {
+        if(node) {
+            postorder(node.left);
+            postorder(node.right);
+            list.push(node.val);
+        }
+    }
+    if(root)  postorder(root);
+    return list;
+};
+
