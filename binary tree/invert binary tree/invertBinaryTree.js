65aae48556be0dc607eb1bbca55b0a012927c521
Ken <67538811+YinHk@users.noreply.github.com>
2022-07-02 00:09:13 -0400

    Create invertBinaryTree.js

---
+++ binary tree/invert binary tree/invertBinaryTree.js
@@ -0,0 +1,16 @@
+const invertTree = = (root) => {
+   
+   const swapTree = (node) => {
+      if(node){
+        let temp = node.right;
+        node.right = node.left;
+        node.left = temp;
+        swapTree(node.left);
+        swapTree(node.right)
+      }
+   }
+   swapTree(root);
+   return root;
+};
+
+
