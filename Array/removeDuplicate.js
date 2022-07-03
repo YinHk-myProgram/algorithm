985b8ce2f2c48e8ca63da7ed50199235f446cfca
Ken <67538811+YinHk@users.noreply.github.com>
2022-07-03 17:40:44 -0400

    Create removeDuplicate.js

---
+++ Array/removeDuplicate.js
@@ -0,0 +1,11 @@
+// remove duplicate elements in the array
+const array = [1,2,2,3,3,3,5] 
+const uniq = [...new Set(array)]     // [ 1, 2, 3, 5 ]
+
+
+//or 
+const uniqueArray = array.filter(function(item, pos) {
+    return array.indexOf(item) == pos;
+})
+
+
