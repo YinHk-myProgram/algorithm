4cabc3a0e6172f87059c8c7e418db3e8473e46cb
Ken <67538811+YinHk@users.noreply.github.com>
2022-07-03 15:01:02 -0400

    Update max&min.js

--- Array/Find maximum & minimum/max&min.js
+++ Array/Find maximum & minimum/max&min.js
@@ -1,1 +1,36 @@
+//Find min & max using Math object method
+Math.max(...array)
+Math.min(...array)
+
+
+//using built-in reduce() method
+const arr = [1,2,33,18,39]
+const max = arr.reduce((a, b) => {
+    if(a > b) return a;
+      else return b;
+})
+
+const min = arr.reduce((a, b) => {
+    if(a < b) return a;
+      else return b;
+})
+
+
+//using loop
+let min = arr[0]
+for (let i = 1; i < arr.length; ++i) {
+    if (arr[i] < min) {
+        min = arr[i]
+    }
+}
+return min
+
+let max = arr[0]
+for (let i = 1; i < arr.length; ++i) {
+    if (arr[i] < max) {
+        max = arr[i]
+    }
+}
+return max
+
 
