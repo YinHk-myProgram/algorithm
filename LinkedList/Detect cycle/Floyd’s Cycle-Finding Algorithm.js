8d384ffee7234583e8fa0d024e97eb45cbe8d865
Ken <67538811+YinHk@users.noreply.github.com>
2022-08-06 01:24:29 -0400

    Create Floyd’s Cycle-Finding Algorithm.js

---
+++ LinkedList/Detect cycle/Floyd’s Cycle-Finding Algorithm.js
@@ -0,0 +1,48 @@
+let head; // head of list
+ 
+/* Linked list Node*/
+class Node
+{
+    constructor(d)
+    {
+        this.data = d;
+        this.next = null;
+    }
+}
+ 
+/* Inserts a new Node at front of the list. */
+function push(new_data)
+{
+     
+    /* 1 & 2: Allocate the Node &
+            Put in the data*/
+    let new_node = new Node(new_data);
+ 
+    /* 3. Make next of new Node as head */
+    new_node.next = head;
+ 
+    /* 4. Move the head to point to new Node */
+    head = new_node;
+}
+ 
+function detectLoop()
+{
+    let slow_p = head, fast_p = head;
+    let flag = 0;
+     
+    while (slow_p != null && fast_p != null &&
+           fast_p.next != null)
+    {
+        slow_p = slow_p.next;
+        fast_p = fast_p.next.next;
+        if (slow_p == fast_p)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1)
+        document.write("Loop found<br>");
+    else
+        document.write("Loop not found<br>");
+}
