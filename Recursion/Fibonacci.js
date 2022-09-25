5d95b97eae1bc1f9649c1d6cc78bbbeda2052d8d
Ken <67538811+YinHk@users.noreply.github.com>
2022-09-25 19:09:44 -0400

    Create Fibonacci.js

---
+++ Recursion/Fibonacci.js
@@ -0,0 +1,12 @@
+// Use recursion to find  Nth Fibonacci Number
+/* The Fibonacci numbers are the numbers in the following integer sequence.
+   0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+   
+   Fn = Fn-1 + Fn-2
+ */ 
+
+function fib(n) {
+    if (n <= 1)
+        return n;
+    return fib(n-1) + fib(n-2);
+}
