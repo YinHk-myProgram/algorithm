0188b81cfbc502647e2ff1e90466be5d054991a1
Ken <67538811+YinHk@users.noreply.github.com>
2022-09-25 20:06:47 -0400

    Update Fibonacci.js

--- Recursion/Fibonacci.js
+++ Recursion/Fibonacci.js
@@ -7,8 +7,25 @@
    Time Complexity: Exponential, as every function calls two other functions.
  */ 
 
+//Solution-1: Recursion-Top-Bottom Approach
 function fib(n) {
     if (n <= 1)
         return n;
     return fib(n-1) + fib(n-2);
 }
+
+//Solution-2: Recursion with memoization, Optimization
+// Recursion with memoization
+ function fibMemo(n, memo = {}) {
+     if (memo[n]) {
+         return memo[n];
+     } else {
+         if (n == 1 || n == 2) {
+             memo[n] = 1;
+         } else {
+             memo[n] = fibMemo((n - 1), memo) + fibMemo((n - 2), memo);
+         }
+     }
+     return memo[n];
+ }
+
