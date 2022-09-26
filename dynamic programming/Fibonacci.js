8bf7ddffc70a643b5b9ef12216a13c13af00fddc
Ken <67538811+YinHk@users.noreply.github.com>
2022-09-25 20:04:20 -0400

    Create Fibonacci.js

---
+++ dynamic programming/Fibonacci.js
@@ -0,0 +1,14 @@
+// use dynamic programming to find the Nth fibonacci number
+const fibonacci = num => {
+  let a = 1, b = 0, temp;
+
+  while (num >= 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+  }
+
+  return b;
+}
+
