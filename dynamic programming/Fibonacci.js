2d9f92ba7da89850c47ae2740ae819d4a13bf65b
Ken <67538811+YinHk@users.noreply.github.com>
2022-09-25 20:09:51 -0400

    Update Fibonacci.js

--- dynamic programming/Fibonacci.js
+++ dynamic programming/Fibonacci.js
@@ -12,3 +12,14 @@ const fibonacci = num => {
   return b;
 }
 
+//Dynamic Programming- Bottom-Up Approach
+const fibBottomUp = (n, bottom_up = []) => {
+     bottom_up[1] = 1;
+     bottom_up[2] = 1;
+     for (let i = 3; i <= n; i++) {
+         bottom_up[i] = bottom_up[i - 1] + bottom_up[i - 2];
+     }
+     // bottom_up[]= 1, 2, 2, 3, 5.... till n
+     // console.log(bottom_up);
+     return bottom_up[n];
+ }
