08489ad93139d09ee8a0135b5cbe1f652352a88c
Ken <67538811+YinHk@users.noreply.github.com>
2022-11-05 23:13:53 -0400

    Create sort by date.js

---
+++ sorting/sort by date.js
@@ -0,0 +1,17 @@
+//Sort an array by date 
+
+const d1 = new Date('2019-06-01');
+const d2 = new Date('2018-06-01');
+const d3 = new Date('2019-06-01');
+
+const objects = [
+  { createdAt: d1, name: 'Test 1' },
+  { createdAt: d2, name: 'Test 2' },
+  { createdAt: d3, name: 'Test 3' }
+];
+
+objects.sort((a, b) => a.createdAt - b.createdAt);
+
+console.log(objects.map(o => o.name));    // [ 'Test 2', 'Test 1', 'Test 3' ]
+
+
