4e5bf4a2965a7f08d03fdd9cdcf5df503d4ad293
Ken <67538811+YinHk@users.noreply.github.com>
2022-11-06 21:31:12 -0500

    Update threeSum.js

--- Array/sum/Three Sum/threeSum.js
+++ Array/sum/Three Sum/threeSum.js
@@ -1,1 +1,55 @@
+const threeSum = function(array) {
+     array.sort((a,b) => a - b);
+    const triplets = [];
 
+    for(let i=0; i < array.length - 2; i++){
+        if(array[i] != array[i-1]){ // making sure our solution set does not contain duplicate triplets
+            let left = i + 1;
+          let right = array.length - 1;
+
+            while (left < right){
+                const currentSum = array[i] + array[left] + array[right];
+                if (currentSum === 0){
+                    triplets.push([array[i], array[left], array[right]]);
+                    while(array[left] == array[left + 1]) left ++
+                    while(array[right] == array[right - 1]) right -- // making sure our solution set does not contain duplicate triplets
+                    left ++;
+                    right --;
+                } else if(currentSum < 0) {
+                    left ++
+                } else if(currentSum > 0){
+                    right --
+                }
+            }
+        }
+    }
+    return triplets
+};
+
+// or 
+const threeSum = function(nums) {
+  var len = nums.length;
+  var res = [];
+  var l = 0;
+  var r = 0;
+  nums.sort((a, b) => (a - b));
+  for (var i = 0; i < len; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    l = i + 1;
+    r = len - 1;
+    while (l < r) {
+      if (nums[i] + nums[l] + nums[r] < 0) {
+        l++;
+      } else if (nums[i] + nums[l] + nums[r] > 0) {
+        r--;
+      } else {
+        res.push([nums[i], nums[l], nums[r]]);
+        while (l < r && nums[l] === nums[l + 1]) l++;
+        while (l < r && nums[r] === nums[r - 1]) r--;
+        l++;
+        r--;
+      }
+    }
+  }
+  return res;
+};
